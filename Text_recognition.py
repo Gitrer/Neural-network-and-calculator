0083cc88b3f0ecb1acd283ec7735ef4e3bcde09a
Gitrer <143604731+Gitrer@users.noreply.github.com>
2023-08-31 23:20:52 +1100

    Update Text_recognition.py

--- Text_recognition.py
+++ Text_recognition.py
@@ -2,7 +2,7 @@ import sys
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 Image_filename = sys.argv[1]
 Image = Image.open(Image_filename)
